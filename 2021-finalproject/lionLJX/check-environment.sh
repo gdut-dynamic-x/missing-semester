e7ff6a78b4889e5c8fd188c50c22e456d7297870
JX <3435574021@qq.com>
2021-11-08 00:40:04 +0800

    update check-environment.sh file #2

--- 2021-finalproject/lionLJX/check-environment.sh
+++ 2021-finalproject/lionLJX/check-environment.sh
@@ -8,7 +8,18 @@ if [ $RETURN == "0" ];then
 	echo  " pass"
 else 
 	echo  "This environment variable is empty!"
+	exit 0
 fi
 
 #check the environment variable whether have a valid address
+	printf  "[check] address validation..."
+if [ $ROS_MASTER_URI == 'http://localhost:11311' ];then
+	echo  "pass"
+else 
+	echo  "The address isn't valid"
+fi
+
+#ping address to check ip
+ping -c 1 $ROS_IP
+
 
