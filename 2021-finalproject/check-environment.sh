fcce9b6534cda789b0a52795cd851c3d82d52258
handsome <1647368943@qq.com>
2021-11-08 15:55:42 +0800

    update script

--- 2021-finalproject/check-environment.sh
+++ 2021-finalproject/check-environment.sh
@@ -1,11 +1,18 @@
+
 #!/bin/sh
 #check ROS_MASTER_URI
 export ROS_MASTER_URI=http://192.168.0.100:11311
-echo -n "[check] environment varible..."
+echo -n "[check] environment varible... "
 if test -z "${ROS_MASTER_URI}";
 then
-    echo "environment varible of ROS_MASTER_URI is null"
+    echo "environment varible of ROS_MASTER_URI is unnull"
 else
-    echo "environment varible of ROS_MASTER_URI is unnul"
+    echo "Pass"
 fi
-
+#check http
+check=$(export | grep ROS_MASTER_URI | sed -E "s/.*[0-9]{3}.[0-9]{3}.[0-9].[0-9]{3}.*//")
+if [ $check -eq ''];then
+    echo "[check] address validation...  Pass"
+else
+    echo "[check] ping address...        False"
+fi 
