2ebe9726d2083fd9c08f7d1f413ddcdccf6ccff3
handsome <1647368943@qq.com>
2021-11-09 14:09:47 +0800

    update check-environment.sh

--- 2021-finalproject/check-environment.sh
+++ 2021-finalproject/check-environment.sh
@@ -9,10 +9,35 @@ then
 else
     echo "Pass"
 fi
+
+
 #check http
-check=$(export | grep ROS_MASTER_URI | sed -E "s/.*[0-9]{3}.[0-9]{3}.[0-9].[0-9]{3}.*//")
-if [ $check -eq ''];then
-    echo "[check] address validation...  Pass"
+check=$(export | grep ROS_MASTER_URI | sed -E "s/([0-9]{3}.[0-9]{3}.[0-9].[0-9]{3})/\1/")
+if [ $Check -eq ''];then
+    echo "[Check] address validation...  Pass"
 else
-    echo "[check] ping address...        False"
+    echo "[Check] ping validation...     False"
 fi 
+
+
+#check ping
+ip=$(export | grep ROS_MASTER_URI | sed -E "s/.*([0-9]{3}.[0-9]{3}.[0-9].[0-9]{3}).*/\1/")
+total="0"
+line="0"
+while [ "$total" -le 5 ]; do
+    line='ping $ip -c 1 -s 1 -W 1 | grep "100% packet loss" |wc -l'
+    if [ "${line}" != "0" ]; then
+        total=$((total+1))
+    else
+        total="0"
+        echo "[Check] ping address...        Pass"
+    fi
+    if [ "${total}" == "5" ]; then
+        echo "[Check] ping address...        False"
+    fi
+done        
+
+
+#check subnet
+subnet=$(ifconfig | grep -E "netmask" | sed -E "s/([0-9]{3}.[0-9]{3}.[0-9]{3}.[0-9]){1}/\1/")
+echo $subnet
