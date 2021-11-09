c6da21a7159e4c733ef1d00161ec546298d0d9b0
lht <LHT13535837097@163.com>
2021-11-09 23:34:20 +0800

    add sriptPNG

--- _2021-finalproject/LHTcode/check_environment.sh
+++ _2021-finalproject/LHTcode/check_environment.sh
@@ -7,13 +7,15 @@ if [[ -n $ROS_MASTER_URI ]];then
 	environment=$ROS_MASTER_URI
 else
 	echo "[Check] environment variable... Faile"
+	exit -1
 fi
 echo $environment > ~/cache.txt
-if [[ $(cat ~/cache.txt | sed -E 's/http:\/\/.*:.*$//') -eq "" ]];then
-	echo "[Check] address validation...   Pass"
-	ping $(cat ~/cache.txt|sed -E 's/http:\/\/(.*):.*$/\1/')
-       	rm ~/cache.txt	
+if [[ $(cat ~/cache.txt | sed -E 's/http:\/\/.*:.*$//') -eq "" ]];then	
+	echo "[Check] address validation...   Pass" 		
 else
 	echo "[Check] address validation...   Faile"
 	rm ~/cache.txt
+	exit -1
 fi
+ping $(cat ~/cache.txt | sed -E 's/http:\/\/(.*):.*$/\1/') 
+rm ~/cache.txt
